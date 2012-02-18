b4cb9397fdeb37af26cd6babd05cc1ba01427561
Artem <artem.cpt@gmail.com>
2012-02-18 18:56:47 +0400

    refactoring

--- radar.js
+++ radar.js
@@ -1,4 +1,52 @@
 
+var Filter = {
+
+    parse: function(data) {
+        return data.map(function(e) {
+            return {
+                acc: {
+                    x: parseFloat(e.acc.x) * 9.8,
+                    y: parseFloat(e.acc.y) * 9.8
+                },
+                gps: {
+                    direction: parseFloat(e.gps.direction), 
+                    speed: parseFloat(e.gps.speed)
+                },
+                timestamp: parseInt(e.timestamp)
+            };
+        });
+    },
+
+    movingAverage: function(samples, k) {
+        var res = samples.map(function(e) {
+            return $.extend(true, {}, e);
+        }); // Making deep copy
+
+        for (var i = 0; i < samples.length; i++) {
+            
+            var sum_x = 0,
+                sum_y = 0;
+            
+            var slice = samples.slice(Math.max(0, i - k), Math.min(samples.length, i + k + 1));
+            slice.forEach(function(e) {
+                sum_x += e.acc.x;
+                sum_y += e.acc.y;
+            });
+
+            res[i].acc.x = sum_x / slice.length; 
+            res[i].acc.y = sum_y / slice.length;
+        }
+        return res;
+    },
+
+    accModule: function(samples) {
+        return samples.map(function(s) {
+            var a = Math.sqrt(Math.pow(s.acc.x, 2) + Math.pow(s.acc.y, 2));
+            return $.extend(true, { acc: {'a' : a }}, s);
+        });
+    }
+};
+
 var Sampler = (function() {
     
     var current_sample_index,
@@ -15,51 +63,7 @@ var Sampler = (function() {
 
     return {
         init: function(data) {
-
-            /* convert to valid data */
-            samples = data.map(function(e) {
-                return {
-                    acc: {
-                        x: parseFloat(e.acc.x) * 9.8,
-                        y: parseFloat(e.acc.y) * 9.8
-                    },
-                    gps: {
-                        direction: parseFloat(e.gps.direction), 
-                        speed: parseFloat(e.gps.speed)
-                    },
-                    timestamp: parseInt(e.timestamp)
-                };
-            });
-            
-            /* slice middle */
-            samples = (function() { 
-                var res = samples.map(function(e) {
-                    return $.extend(true, {}, e);
-                }); // Making deep copy
-
-                var k = 3;
-                for (var i = 0; i < samples.length; i++) {
-                    
-                    var sum_x = 0,
-                        sum_y = 0;
-                    
-                    var slice = samples.slice(Math.max(0, i - k), Math.min(samples.length, i + k + 1));
-                    slice.forEach(function(e) {
-                        sum_x += e.acc.x;
-                        sum_y += e.acc.y;
-                    });
-
-                    res[i].acc.x = sum_x / slice.length; 
-                    res[i].acc.y = sum_y / slice.length;
-                }
-                return res;
-            })();
-
-            samples.forEach(function(s) {
-                s.acc.a = Math.sqrt(Math.pow(s.acc.x, 2) + Math.pow(s.acc.y, 2));
-            });
-
-            console.log(samples);
+            samples = data; 
             this.reset();
         },
     
@@ -183,7 +187,12 @@ var Drawer = (function() {
 var timer;
 
 $().ready(function () {
-    Sampler.init(data);
+
+    var samples = Filter.parse(data);
+    samples = Filter.movingAverage(samples, 30);
+    samples = Filter.accModule(samples);
+
+    Sampler.init(samples);
     Drawer.init();
     
     $('#canvas').click(function() {
